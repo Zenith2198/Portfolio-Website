5506508cd022df136c8eac5346d2af6af4813ad3
Carter Rollins <croll2198@gmail.com>
2023-09-21 19:35:16 -0400

    Updates eslintrc for Vercel

--- middleware.tsx
+++ middleware.tsx
@@ -3,10 +3,10 @@ import { NextResponse } from "next/server";
 
 export default withAuth(
 	function middleware(request: NextRequestWithAuth) {
-		if ((request.nextUrl.pathname.startsWith("/admin") || request.nextUrl.pathname.startsWith("/api/admin")) && request.nextauth.token?.role !== "admin") {
+		if ((request.nextUrl.pathname.startsWith("/admin") || request.nextUrl.pathname.startsWith("/api/posts/admin")) && request.nextauth.token?.role !== "admin") {
 			return NextResponse.rewrite(new URL("/denied", request.url));
 		}
 	}
 );
 
-export const config = { matcher: ["/admin", "/api/admin"] };
+export const config = { matcher: ["/admin", "/api/posts/admin"] };
