2279bcff91254153326f8fc283513ca16547a05e
Carter Rollins <croll2198@gmail.com>
2023-09-24 15:30:51 -0400

    Changes vercel url

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -11,7 +11,7 @@ export function getBaseUrl() {
 	if (process.env.NODE_ENV === "development") {
 		return "http://localhost:3000";
 	}
-	return `https://google.com`;
+	return `https://${process.env.VERCEL_URL}`;
 }
 
 export function fixDate(unixTimestamp: number) {
