e9bb4c7f5b2f3007829320dff5813d3fbf1e9176
Carter Rollins <croll2198@gmail.com>
2023-09-24 15:02:46 -0400

    Removes window from baseUrl

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -5,9 +5,9 @@ export const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export function getBaseUrl() {
 	if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production")
-		return `https://${window.location.origin}`;
+		return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 	if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview")
-		return `https://${window.location.origin}`;
+		return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 	return "http://localhost:3000";
 }
 
