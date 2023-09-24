34fc84343b84ebfb4509cb50790d17843d72e8b3
Carter Rollins <croll2198@gmail.com>
2023-09-24 16:36:57 -0400

    Comments out all fetch requests in server

--- app/(dashboard)/[postType]/[path]/layout.tsx
+++ app/(dashboard)/[postType]/[path]/layout.tsx
@@ -2,13 +2,13 @@ import { getBaseUrl, fixDate, buildURLParams } from "@/lib/utils";
 import Image from "next/image";
 
 export default async function PostLayout({ children, params }: { children: React.ReactNode, params: { path: string, chapterNum: string } }) {
-	const postDataRes = await fetch(`${getBaseUrl()}/api/posts/${params.path}`);
-	if (!postDataRes.ok) return <div>Error</div>;
-	const postData = await postDataRes.json();
+	// const postDataRes = await fetch(`${getBaseUrl()}/api/posts/${params.path}`);
+	// if (!postDataRes.ok) return <div>Error</div>;
+	// const postData = await postDataRes.json();
 
 	return (
 		<div className="flex-1 grid grid-cols-4 gap-5 m-5">
-			<div>
+			{/* <div>
 				<div className="card lg:card-side bg-base-100 shadow-xl">
 					{postData.image ? 
 						<figure><Image src={postData.image} alt="" fill={true} className="object-contain max-h-[32rem]" /></figure>
@@ -24,7 +24,7 @@ export default async function PostLayout({ children, params }: { children: React
 			</div>
 			<div className="col-span-3">
 				{children}
-			</div>
+			</div> */}
 		</div>
 	);
 }
--- app/(dashboard)/[postType]/[path]/page.tsx
+++ app/(dashboard)/[postType]/[path]/page.tsx
@@ -4,25 +4,25 @@ import { getBaseUrl, buildURLParams } from "@/lib/utils";
 export const dynamicParams = false;
 
 export default async function Post({ params }: { params: { postType: string, path: string } }) {
-	const urlQuery = buildURLParams({ chapters: true });
-	const postDataRes = await fetch(`${getBaseUrl()}/api/posts/${params.path}?${urlQuery}`);
-	if (!postDataRes.ok) return <div>Error</div>;
-	const postData = await postDataRes.json();
+	// const urlQuery = buildURLParams({ chapters: true });
+	// const postDataRes = await fetch(`${getBaseUrl()}/api/posts/${params.path}?${urlQuery}`);
+	// if (!postDataRes.ok) return <div>Error</div>;
+	// const postData = await postDataRes.json();
 
-	if (postData.chapters.length === 0) {
-		return (
-			<div className="card bg-base-100 shadow-xl text-9xl p-16 text-center">There is nothing here yet!</div>
-		);
-    } else if (postData.chapters.length > 1) {
-        redirect(`${getBaseUrl()}/${params.postType}/${params.path}/${postData.chapters[0].chapterNum}`);
-    }
+	// if (postData.chapters.length === 0) {
+	// 	return (
+	// 		<div className="card bg-base-100 shadow-xl text-9xl p-16 text-center">There is nothing here yet!</div>
+	// 	);
+    // } else if (postData.chapters.length > 1) {
+    //     redirect(`${getBaseUrl()}/${params.postType}/${params.path}/${postData.chapters[0].chapterNum}`);
+    // }
 
 	return (
 		<div>
-			<div className="card bg-base-100 shadow-xl p-10">
+			{/* <div className="card bg-base-100 shadow-xl p-10">
 				<h1 className="text-5xl pb-5">{postData.chapters[0].title}</h1>
 				<div className="m-5" dangerouslySetInnerHTML={{ __html: postData.chapters[0].content }} />
-			</div>
+			</div> */}
 		</div>
 	);
 }
