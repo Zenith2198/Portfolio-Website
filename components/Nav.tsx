5cb81c81730905f4dc296d97dc4bf40badb98288
Carter Rollins <croll2198@gmail.com>
2023-09-24 19:00:34 -0400

    Tests nav as server component

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,4 @@
-"use client"
+
 
 import Link from "next/link";
 import type { Post } from "@prisma/client";
@@ -6,9 +6,7 @@ import useSWR from "swr";
 import { fetcher, getBaseUrl } from "@/lib/utils";
 
 export default function Nav({ urlQuery }: { urlQuery: string }) {
-	const { data, isLoading, error } = useSWR(`${getBaseUrl()}/api/posts`, fetcher);
-	console.log(`${getBaseUrl()}/api/posts?${urlQuery}`)
-	console.log(error)
+	const { data, isLoading, error } = useSWR(`${getBaseUrl()}/api/posts?${urlQuery}`, fetcher);
 	if (isLoading) return <div>Loading...</div>;
   	if (error) return <div>Error</div>;
 
@@ -42,37 +40,37 @@ export default function Nav({ urlQuery }: { urlQuery: string }) {
 
 	return (
 		<ul className="menu menu-horizontal">
-			<li onClick={unfocus} key="longWorks" className="dropdown dropdown-hover">
+			<li key="longWorks" className="dropdown dropdown-hover">
 				<label className="btn m-1 flex flex-col text-2xl">
 					<Link href={`${getBaseUrl()}/long-stories`} className="break-words">Long Works</Link>
 				</label>
 				<ul className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box">
 					{longStories.map(({ title, path }, i) => (
-						<li onClick={unfocus} key={i}>
+						<li key={i}>
 							<Link href={`${getBaseUrl()}/long-stories/${path}`}>{title}</Link>
 						</li>
 					))}
 				</ul>
 			</li>
-			<li onClick={unfocus} key="shortWorks" className="dropdown dropdown-hover">
+			<li key="shortWorks" className="dropdown dropdown-hover">
 				<label className="btn m-1 flex flex-col text-2xl">
 					<Link href={`${getBaseUrl()}/short-stories`} className="break-words">Short Works</Link>
 				</label>
 				<ul className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box">
 					{shortStories.map(({ title, path }, i) => (
-						<li onClick={unfocus} key={i}>
+						<li key={i}>
 							<Link href={`${getBaseUrl()}/short-stories/${path}`}>{title}</Link>
 						</li>
 					))}
 				</ul>
 			</li>
-			<li onClick={unfocus} key="blogs" className="dropdown dropdown-hover">
+			<li key="blogs" className="dropdown dropdown-hover">
 				<label className="btn m-1 flex flex-col text-2xl">
 					<Link href={`${getBaseUrl()}/blogs`} className="break-words ">Blogs</Link>
 				</label>
 				<ul className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box">
 					{blogs.map(({ title, path }, i) => (
-						<li onClick={unfocus} key={i}>
+						<li key={i}>
 							<Link href={`${getBaseUrl()}/blogs/${path}`}>{title}</Link>
 						</li>
 					))}
