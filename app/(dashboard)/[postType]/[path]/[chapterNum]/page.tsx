1b7cb1962f01b740d1d0081d561ef1eb18944e05
Carter Rollins <croll2198@gmail.com>
2023-09-24 16:22:47 -0400

    Test moving fetch requests into components

--- app/(dashboard)/[postType]/[path]/[chapterNum]/page.tsx
+++ app/(dashboard)/[postType]/[path]/[chapterNum]/page.tsx
@@ -1,21 +1,13 @@
-import { getBaseUrl, buildURLParams } from "@/lib/utils";
 import ChapterNav from "@/components/ChapterNav";
+import ChapterDisplay from "@/components/ChapterDisplay";
 
 export const dynamicParams = false;
 
 export default async function Chapter({ params }: { params: { path: string, chapterNum: string } }) {
-	const urlQuery = buildURLParams({ chapters: true  });
-	const postDataRes = await fetch(`${getBaseUrl()}/api/posts/${params.path}?${urlQuery}`);
-	if (!postDataRes.ok) return <div>Error</div>;
-	const postData = await postDataRes.json();
-
 	return (
 		<div>
 			<ChapterNav path={params.path} chapterNum={params.chapterNum}/>
-			<div className="card bg-base-100 shadow-xl p-10 my-3">
-				<h1 className="text-5xl pb-5">{postData.chapters[Number(params.chapterNum) - 1].title}</h1>
-				<div className="m-5" dangerouslySetInnerHTML={{ __html: postData.chapters[Number(params.chapterNum) - 1].content }} />
-			</div>
+			<ChapterDisplay path={params.path} chapterNum={params.chapterNum}/>
 			<ChapterNav className="dropdown-top" path={params.path} chapterNum={params.chapterNum}/>
 		</div>
 	);
--- lib/utils.tsx
+++ lib/utils.tsx
@@ -8,9 +8,9 @@ export async function fetcher(url: string) {
 }
 
 export function getBaseUrl() {
-	// if (process.env.NODE_ENV === "development") {
-	// 	return "http://localhost:3000";
-	// }
+	if (process.env.NODE_ENV === "development") {
+		return "http://localhost:3000";
+	}
 	return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 }
 
