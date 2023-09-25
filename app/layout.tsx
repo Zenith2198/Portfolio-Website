86f95794564d0f1bae9d28886c9ffc7831e14b00
Carter Rollins <croll2198@gmail.com>
2023-09-24 22:37:35 -0400

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,16 @@
 
 import "./globals.css";
 import AuthProvider from "./context/AuthProvider";
+import { Suspense } from "react";
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" className="min-w-screen min-h-screen overflow-x-hidden m-0 p-0">
 			<body className="min-w-screen min-h-screen overflow-x-hidden m-0 p-0">
 				<AuthProvider>
-					{children}
+					<Suspense>
+						{children}
+					</Suspense>
 				</AuthProvider>
 			</body>
 		</html>
