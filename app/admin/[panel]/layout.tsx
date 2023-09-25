61400e74c0f55d634cb52f9799bc5f83587f9daf
Carter Rollins <croll2198@gmail.com>
2023-09-24 23:42:09 -0400

    Moves client to page from layout

--- app/admin/[panel]/layout.tsx
+++ app/admin/[panel]/layout.tsx
@@ -1,25 +1,7 @@
-"use client"
-
-import type { ChangeEvent } from "react";
-import { useRouter } from "next/navigation";
-import { getBaseUrl } from "@/lib/utils";
-
 export default function PanelLayout({ children, params }: { children: React.ReactNode, params: { panel: string } }) {
-	const router = useRouter();
-
-	const handleOptionSelect = (event: ChangeEvent<HTMLInputElement>) => {
-		router.push(`${getBaseUrl()}/admin/${event.target.value}`);
-	};
-
 	return (
 		<div>
-			<div className="join" onChange={handleOptionSelect}>
-				<input value="newPost" checked={params.panel === "newPost"} readOnly className="join-item btn" type="radio" name="options" aria-label="New Post"/>
-				<input value="editPost" checked={params.panel === "editPost"} readOnly className="join-item btn" type="radio" name="options" aria-label="Edit Post"/>
-			</div>
-			<div>
-				{children}
-			</div>
+			{children}
 		</div>
 	);
 }
--- app/admin/[panel]/page.tsx
+++ app/admin/[panel]/page.tsx
@@ -1,11 +1,26 @@
+"use client"
+
 import NewPostPanel from "@/components/NewPostPanel";
 import EditPostPanel from "@/components/EditPostPanel";
+import type { ChangeEvent } from "react";
+import { useRouter } from "next/navigation";
+import { getBaseUrl } from "@/lib/utils";
 
 export const dynamicParams = false;
 
 export default function Panel({ params }: { params: { panel: string } }) {
+	const router = useRouter();
+
+	const handleOptionSelect = (event: ChangeEvent<HTMLInputElement>) => {
+		router.push(`${getBaseUrl()}/admin/${event.target.value}`);
+	};
+
 	return (
 		<div>
+			<div className="join" onChange={handleOptionSelect}>
+				<input value="newPost" checked={params.panel === "newPost"} readOnly className="join-item btn" type="radio" name="options" aria-label="New Post"/>
+				<input value="editPost" checked={params.panel === "editPost"} readOnly className="join-item btn" type="radio" name="options" aria-label="Edit Post"/>
+			</div>
 			<NewPostPanel className={`${params.panel==="newPost"?"block":"hidden"}`}/>
 			<EditPostPanel className={`${params.panel==="editPost"?"block":"hidden"}`}/>
 		</div>
