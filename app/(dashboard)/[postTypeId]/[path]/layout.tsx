f221ae990196870955a2b28643ea8b4016255bce
Carter Rollins <croll2198@gmail.com>
2023-09-26 17:12:13 -0400

    Adds component for post side card to fix loading

--- app/(dashboard)/[postTypeId]/[path]/layout.tsx
+++ app/(dashboard)/[postTypeId]/[path]/layout.tsx
@@ -1,35 +1,12 @@
-import { fixDate } from "@/lib/utils";
 import { postFindUnique } from "@/lib/db";
-import Image from "next/image";
+import PostSideCard from "@/components/PostSideCard";
 
 // export const dynamicParams = false;
 
-export default async function PostLayout({ children, params }: { children: React.ReactNode, params: { path: string, chapterNum: string } }) {
-	let path = params.path;
-	if (process.env.NODE_ENV !== "development") {
-		path = decodeURIComponent(params.path);
-	}
-	const post = await postFindUnique({
-		where: {
-			path
-		}
-	});
-	if (!post) return <div>Error</div>;
-
+export default async function PostLayout({ children, params }: { children: React.ReactNode, params: { path: string } }) {
 	return (
 		<div className="flex-1 grid grid-cols-4 gap-5 m-5">
-			<div>
-				<div className="card lg:card-side bg-base-100 shadow-xl">
-					{post.imageLink ? 
-						<figure><Image src={post.imageLink} alt="" className="object-contain max-h-lg" width={512} height={100} /></figure>
-					: <div></div>
-					}
-					<div className="card-body">
-						<h2 className="card-title">{post?.title}</h2>
-						<h1 className="text-info text-xl pb-2 mb-5">{fixDate(post.dateModified)}</h1>
-					</div>
-				</div>
-			</div>
+			<PostSideCard path={params.path}/>
 			<div className="col-span-3">
 				{children}
 			</div>
